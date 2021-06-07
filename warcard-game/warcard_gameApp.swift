b4709f7479e136f96e570b9605218d7210d60002
Alisher <Alisher>
2021-06-07 15:16:02 +0600

    Initial Commit

---
+++ warcard-game/warcard_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  warcard_gameApp.swift
+//  warcard-game
+//
+//  Created by Алишер Алсейт on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct warcard_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
